dcbca3d8e7da32e5b7e4eda8a9234e7258b67158
Kayla Kelly <76846236+Kaylawkelly@users.noreply.github.com>
2021-04-16 13:46:22 -0400

    commit

---
+++ ETL_Project_Postgres.sql
@@ -0,0 +1,48 @@
+CREATE TABLE Energy(
+State VARCHAR(30),
+Coal FLOAT,
+"Natural Gas" FLOAT,
+"Distillate Fuel Oil" FLOAT,
+HGL	FLOAT,
+"Jet Fuel" FLOAT,
+"Motor Gasoline" FLOAT,	
+"Residual Fuel Oil" FLOAT,	
+"Petrolium Other" FLOAT,
+"Total Petrolium" FLOAT,	
+"Nuclear Fuel" FLOAT,	
+"Wood and Waste" FLOAT,
+"Primary Energy Total" FLOAT,	
+"Electric Power Sector" FLOAT,
+"Electric Retail Sales" FLOAT)
+
+SELECT * FROM energy;
+
+CREATE TABLE population(
+State VARCHAR(30),
+"2018 Population" INTEGER)
+SELECT * FROM population;
+
+CREATE TABLE SR(
+Code VARCHAR(30),
+"Production Share" FLOAT,	
+"Production, Rank" FLOAT,	
+"Consumption, Million Btu" FLOAT,	
+"Consumption, Rank" FLOAT,	
+"Expenditures, Dollars" FLOAT,	
+"Expenditures, Rank" FLOAT
+)
+SELECT * FROM SR;
+
+CREATE TABLE State_Code(
+State VARCHAR(30),
+Code VARCHAR(30)
+	)
+	
+SELECT * FROM State_Code;
+
+CREATE TABLE TEC(
+"Consumption Rank" INTEGER,
+Code VARCHAR(30),
+"Total Energy Consumed per Capita, million Btu" FLOAT)
+
+SELECT * FROM TEC;
